bf049c9eed8b57feb73553e6ad522a87f4d2154d
橘子 <Cheer@Cheer.local>
2017-01-03 16:24:31 +0800

    update for linux

--- Package.swift
+++ Package.swift
@@ -1,13 +1,14 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-07 03:08:04 +0000
+// Date: 2016-12-30 08:58:52 +0000
 import PackageDescription
 let package = Package(
-    name: "PerfectTemplate",
+    name: "swift-sever",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-MongoDB.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Notifications.git", majorVersion: 2, minor: 0),
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Notifications.git", majorVersion: 2, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2, minor: 0),
     ]
 )
